e06fc5b3acd21a0568d3b9af59a2a4a29dc00277
cosyntxd <110003521+cosyntxd@users.noreply.github.com>
2023-01-27 22:03:43 -0800

    Added code
    I had the code written a week ago and recently came back to it to fix formatting and upload it to GitHub. Hypixel, within that time, decided to remove the endpoint, and now it no longer works.

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-# Hypixel Friend Finder
+# Hypixel Friend Search
 Searches through your social network in Hypixel and finds the shortest path to contact someone.
+
+Hypixel, as of Jan 23, has made the controversial decision to remove the friends endpoint in their API see forum post [here](https://hypixel.net/threads/public-api-removal-of-the-friends-endpoint.5245947/). Hopefully, Hypixel will listen to their community and re-enabled the endpoint. But until then, this project no longer works. Feel free to fork the project and repurpose the code for another game.
 
 # Run:
 ```properties
 pip install -r requirements.txt
 python3 src/main.py
 ```
+Example output: 
+
+`f3c5427a60704550ac3aee273c7c6829 < 36e403a3a57348129617003ab76db317 < bfe8cfbdb449485d950883110e0c2305`
+
 
 # Licence
 See [LICENSE](LICENSE) for details.
--- src/main.py
+++ src/main.py
@@ -1,1 +1,100 @@
-print("Hypixel Friend Search")
+import requests
+import collections
+import pickle
+import time
+import os
+
+# Tested to be slightly more efficient
+session = requests.Session()
+
+class Node:
+    def __init__(self, data, prev=None):
+        self.uuid = data
+        self.prev = prev
+
+    def __repr__(self):
+        return "{} > {}".format(self.uuid, self.prev)
+
+
+class InputError(Exception):
+    def __init__(self, str=""):
+        self.str = "\033[91m{}\033[0m".format(str)
+
+    def __str__(self):
+        return self.str
+
+
+if os.name == "nt":
+    os.system("color")
+
+
+api_key = input("API key: ").strip()
+if len(api_key) != 36:
+    raise InputError("Key must be 36 characters long")
+
+speed = input("requests per minute: ").strip()
+try:
+    speed = int(speed)
+except ValueError:
+    raise InputError("Speed must be an integer")
+
+uuid_self = input("UUID of self: ").strip()
+if len(uuid_self) != 32:
+    raise InputError("UUID must be 32 characters long")
+
+uuid_target = input("UUID of target: ").strip()
+if len(uuid_target) != 32:
+    raise InputError("UUID must be 32 characters long")
+
+save_file = "hfs.save"
+
+if os.path.exists(save_file) and os.path.getsize(save_file) > 1:
+    with open(save_file, "rb") as f:
+        [iterations, to_process, check_map, node_tree] = pickle.load(f)
+else:
+    iterations = 0
+    node_tree = Node(uuid_self)
+    to_process = collections.deque([node_tree])
+    check_map = {}
+
+with open(save_file, "wb") as f:
+    while to_process:
+        iterations += 1
+
+        print(f"{iterations} api calls, {len(to_process)} players known, {len(check_map)} processed")
+
+        time.sleep(60 / float(speed))
+
+        friend = to_process.popleft()
+        if check_map.get(friend.uuid):
+            continue
+
+        api_request = session.get(
+            "https://api.hypixel.net/friends",
+            params = {"key": api_key, "uuid": friend.uuid},
+        )
+        if api_request.status_code != 200:
+            print(api_request.json())
+            print(f"API returned with status code {api_request.status_code} because {api_request.json()['cause']}")
+            # Retry by adding the node back to front of queue
+            to_process.appendleft(friend)
+            continue
+
+        for friend_request in api_request.json()["records"]:
+            if friend_request["uuidSender"] == friend.uuid:
+                person = friend_request["uuidReceiver"]
+            else:
+                person = friend_request["uuidSender"]
+
+            person_node = Node(person, prev=friend)
+
+            if person == uuid_target:
+                print(person_node)
+                exit()
+
+            to_process.append(person_node)
+            check_map[friend] = True
+
+        # Saves around every 8-9 minutes at 120/m
+        if iterations % 1000 == 0:
+            pickle.dump([iterations, to_process, check_map, node_tree], f)
